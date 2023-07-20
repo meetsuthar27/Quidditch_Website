4305c8803961c02d99d2c864f9d1f838031ed1fd
Kalash <kalash.s@ahduni.edu.in>
2023-07-20 20:22:13 +0530

    finally

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,13 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const hbs = require('hbs');
 require("./db/conn");
-
+const Register = require("./models/registers");
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(static_path));
 app.set('view engine', "hbs");
 app.set("views", template_path);
@@ -19,6 +21,83 @@ app.get('/', (req, res) => {
 });
 app.get('/matches', (req, res) => {
     res.render("matches");
+});
+app.get('/teamreg_step1', (req, res) => {
+
+
+    res.render("teamreg_step1");
+});
+app.get('/teamreg_step2', (req, res) => {
+    res.render("teamreg_step2");
+});
+app.get('/teamreg_step3_1', (req, res) => {
+    res.render("teamreg_step3_1");
+});
+app.get('/teamreg_step3_2', (req, res) => {
+    res.render("teamreg_step3_2");
+});
+app.get('/teamreg_step4', (req, res) => {
+    res.render("teamreg_step4");
+});
+
+let registerTeamData = {};
+
+app.post('/teamreg_step1', async (req, res) => {
+
+    try {
+        let password = req.body.password;
+        let confirmpassword = req.body.confirmpassword;
+        if (password === confirmpassword) {
+            for (let key in req.body) {
+                registerTeamData[key] = req.body[key];
+            }
+        }
+        else {
+            res.send("Passwords do not match!");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+app.post('/teamreg_step2', async (req, res) => {
+    try {
+        for (let key in req.body) {
+            registerTeamData[key] = req.body[key];
+        }
+        res.send("Noo")
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+});
+app.post('/teamreg_step3_1', async (req, res) => {
+
+    try {
+        for (let key in req.body) {
+            registerTeamData[key] = req.body[key];
+        }
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
+app.post('/teamreg_step3_2', async (req, res) => {
+
+    try {
+        for (let key in req.body) {
+            registerTeamData[key] = req.body[key];
+        }
+    } catch (error) {
+        res.status(400).send(error)
+    }
+});
+app.post('/teamreg_step4', async (req, res) => {
+    try {
+        let registerTeam = new Register(registerTeamData)
+        const registered = await registerTeam.save()
+    } catch (error) {
+        res.status(400).send(error)
+    }
 });
 app.get('/standings', (req, res) => {
     res.render("standings");
---
+++ backend/src/models/registers.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const RegistrationSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    confirmpassword: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    c1name: {
+        type: String,
+        required: true
+    },
+    c1number: {
+        type: String,
+        required: true
+    },
+    c1age: {
+        type: Number,
+        required: true
+    },
+    c2name: {
+        type: String,
+        required: true
+    },
+    c2number: {
+        type: String,
+        required: true
+    },
+    c2age: {
+        type: Number,
+        required: true
+    },
+    c3name: {
+        type: String,
+        required: true
+    },
+    c3number: {
+        type: String,
+        required: true
+    },
+    c3age: {
+        type: Number,
+        required: true
+    }
+})
+
+const Register = new mongoose.model("Register", RegistrationSchema);
+
+module.exports = Register;
